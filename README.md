e2d612727603e5ca67ec031924c64a052c8f359f
Stephen Lihn <stephen.lihn@atomlp.com>
2024-05-09 21:59:48 -0400

    setup and install

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ and test cases for my paper:
 
 https://arxiv.org/abs/2405.04693
 
+See the test cases for usage examples.
+
+## Install
+
+Please use the github local installation method:
+
+pip install git+git://github.com/slihn/gas-impl.git#egg=gas_impl
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+from gas_impl import __version__
+
+setup(
+    name='gas_impl',
+    version=__version__,
+
+    url='https://github.com/slihn/gas-impl',
+    author='Stephen H. Lihn',
+    author_email='stevelihn@gmail.com',
+
+    py_modules=['gas_impl'],
+    
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'mpmath',
+        'pandarallel',
+        'diskcache',
+    ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: Free for non-commercial use',
+    ],
+)
